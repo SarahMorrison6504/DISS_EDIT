14475eb23c53e6d9fcd9de0d6126dcd4eb678e8f
unknown <you@example.com>
2026-01-21 12:24:09 +0000

    edits to zoo plankton section and beginning of validation

--- plotting script.R
+++ plotting script.R
@@ -12,7 +12,7 @@ nc_close(nc)  # close nc file
 
 p <- plot_var_nc(
   nc_file = ('C:/Users/sarah/OneDrive/Dissertation/DISS(EDIT)/DISS_EDIT/output/output.nc'),
-  var_name = "temp",
+  var_name = "PHS_frp",
   fig_path = NULL,
   reference = "surface",
   max_depth = 25,
@@ -24,5 +24,19 @@ p <- plot_var_nc(
   plot.title = NULL,
   color.palette = "RdYlBu",
   color.direction = -1,
-  zlim = c(0, 25))
+  zlim = c(0, 0.5))
 p + coord_cartesian(ylim = c(25, 0))  # limit to 25m for presentation
+
+## similar method, from https://github.com/GLEON/glmtools/blob/main/R/plot_temp.R
+plot_temp <- function(nc_file='output.nc',fig_path = NULL, reference='surface', legend.title = NULL, 
+                     interval = 0.5, text.size = 12, show.legend = TRUE, 
+                     legend.position = 'right', plot.title = NULL){
+  
+  .Deprecated('plot_var',msg = 'Deprecated. Use `plot_var`, where default is var_name = `temp`')
+  
+  plot_var_nc(nc_file = nc_file, var_name = 'temp', fig_path = fig_path, reference = reference, legend.title = legend.title, 
+              interval = interval, text.size = text.size, show.legend = show.legend, 
+              legend.position = legend.position, plot.title = plot.title)
+  
+}
+plot_temp(nc_file = 'output/output.nc')
---
+++ validation.R
@@ -0,0 +1,16 @@
+## validation
+# downloading LakeEnsemblR
+
+install.packages('LakeEnsemblR')
+
+# install.packages("remotes")
+remotes::install_github("GLEON/rLakeAnalyzer")
+1
+
+remotes::install_github("aemon-j/GLM3r", ref = "v3.1.1")
+remotes::install_github("USGS-R/glmtools", ref = "ggplot_overhaul")
+remotes::install_github("aemon-j/FLakeR", ref = "inflow")
+remotes::install_github("aemon-j/GOTMr")
+remotes::install_github("aemon-j/gotmtools")
+remotes::install_github("aemon-j/SimstratR")
+remotes::install_github("aemon-j/MyLakeR")
